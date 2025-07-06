d3ba4ff5ff364aa14252254c15dbc10c8701981a
praveesh91 <praveesh91@gmail.com>
2025-07-07 01:10:07 +0530

    add cart slice and store

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const defaultState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: defaultState,
+  reducers: {
+    addItem: (state, action) => {
+      console.log(state);
+    },
+    clearCart: (state) => {},
+    removeItem: (state, action) => {},
+    editItem: (state, action) => {},
+  },
+});
+
+export const { addItem, editItem, removeItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
