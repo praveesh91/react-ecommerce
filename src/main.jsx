62b68f997f240f35f9f4e7ea52141f7cbe7da4fc
praveesh91 <praveesh91@gmail.com>
2025-07-07 01:26:33 +0530

    provide store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { ToastContainer } from "react-toastify";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+    <ToastContainer position="top-center" />
+  </Provider>
+);
