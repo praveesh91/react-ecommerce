5df9c7dc981e93462cf286850f0cdfb82aa21b6f
praveesh91 <praveesh91@gmail.com>
2025-07-14 21:17:47 +0530

    restrict checkout for non user

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import { ErrorElement } from "./components";
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
 import { loader as productsLoader } from "./pages/Products";
+import { loader as checkoutLoader } from "./pages/Checkout";
 
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
@@ -58,6 +59,7 @@ const router = createBrowserRouter([
       {
         path: "/checkout",
         element: <Checkout />,
+        loader: checkoutLoader(store),
       },
     ],
   },
