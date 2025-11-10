a440298746e9141114d8fbe097dd73c696e0c6f3
praveesh91 <praveesh91@gmail.com>
2025-11-10 11:44:37 +0530

    update cart item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -20,7 +20,6 @@ const CartItem = ({ cartItem }) => {
       key={cartID}
       className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0"
     >
-      {/* IMAGE */}
       <img
         src={image}
         alt={title}
